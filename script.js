05fc574f27ae94732969ddf26fb2e16c3810e57f
nitant-p <nitantpanicker@gmail.com>
2022-12-18 18:54:22 +0530

    Complete rock paper scissors for 5 round game

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let playerWins = 0;
+let computerWins = 0;
 
 const choices = new Map([
     [0, "Rock"],
@@ -19,16 +21,20 @@ function playRound(computerSelection, playerSelection) {
     if (Math.abs(difference) == 1) {
         if (difference < 0) {
             result = `You win! ${playerSelection} beats ${computerSelection}`;
+            playerWins++;
         } else {
             result = `You lose! ${computerSelection} beats ${playerSelection}`;
+            computerWins++;
         }
     } else if (difference == 0) {
         result = "Tie!";
     } else {
         if (difference < 0) {
             result = `You lose! ${computerSelection} beats ${playerSelection}`;
+            computerWins++;
         } else {
             result = `You win! ${playerSelection} beats ${computerSelection}`;
+            playerWins++;
         }
     }
 
@@ -38,4 +44,18 @@ function playRound(computerSelection, playerSelection) {
 
 function game() {
     
+    playerWins. computerWins = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Please choose your weapon!");
+        let computerSelection = getComputerChoice();
+        console.log(`You chose ${playerSelection}, computer chose ${computerSelection}.
+            ${playRound(computerSelection, playerSelection)}`);
+    }
+
+    if (playerWins > computerWins) {
+        console.log("You won in the end! Wins: " + playerWins);
+    } else {
+        console.log("Looks like you lost... :(  Wins: " + playerWins);
+    }
+
 }
